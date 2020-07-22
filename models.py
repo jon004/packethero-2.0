f1064cf1f39e7ffa9d40a8d9d012927e30928a1b
Jon1287 <45994001+yonJM1267@users.noreply.github.com>
2020-07-22 12:26:23 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
 # DataBase management
-# by Tanisha Babic
 #-----------------------------------------------------------------------------#
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
